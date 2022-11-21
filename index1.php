d71b6a9bd1a32b1559b71b7365000e1f07ebb907
Vipul2202 <105905749+Vipul2202@users.noreply.github.com>
2022-11-21 22:46:40 +0530

    yes

---
+++ index1.php
@@ -0,0 +1,130 @@
+<html>
+    <head>
+        <title>Contactus form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+        <link href='custom.css' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+    <header>
+            <div id="menu-bar" class="fa fa-bars"></div>
+           
+             <a href="index.html" class="logo">FOOTWEAR COLLECTION</a>
+            <nav class="navbar">
+                
+
+                <a href="index.html">Home</a>
+                <a href="Product.html">Product</a>
+                <a href="features.html">Featured</a>
+                <a href="blogs.html">Blog</a>
+                <a href="index1.php">Contact Us</a>
+                <a href="index.php">login</a>
+            </nav>
+            <!--<div class="icons">
+                <img src="cart.png" width="30px" height="30px">
+                <img src="user.jpg" width="30px" height="30px">
+                <img src="heart.png" width="30px" height="30px">-->
+                
+                </div>
+        </header>
+        
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-lg-8 col-lg-offset-2">
+
+                    <h1>Contact form from Footwear Collection</h1>
+
+                    
+
+                    <form id="contact-form" method="post" role="form">
+
+                        <div class="messages"></div>
+
+                        <div class="controls">
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_name">Firstname *</label>
+                                        <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_lastname">Lastname *</label>
+                                        <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_email">Email *</label>
+                                        <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form_phone">Phone</label>
+                                        <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="form_message">Message *</label>
+                                        <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="submit" name="ok" class="btn btn-success btn-send" value="Send message">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+        <?php 
+        require('dbb.php');
+        
+            if(isset($_POST['ok'])){
+                include_once 'function.php';
+                $obj=new Contact();
+                $res=$obj->contact_us($_POST);
+                if($res==true){
+                    echo "<script>alert('Query successfully Submitted.Thank you')</script>";
+                    
+                }else{
+                
+                 echo "<script>alert('Submitted Successfully')</script>";
+                 
+                }
+            }
+            ?>
+
+                </div><!-- /.8 -->
+
+            </div> <!-- /.row-->
+
+        </div> <!-- /.container-->
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="validator.js"></script>
+        <script src="contact.js"></script>
+    </body>
+</html>
